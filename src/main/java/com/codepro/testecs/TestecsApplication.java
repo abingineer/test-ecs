e3cc6c1f2dff630d5c83ff5d96dadf8b4e84ed01
aboubacar <ab_diallo@etu.enset-media.ac.ma>
2025-12-14 20:58:54 +0100

    first commit

--- src/main/java/com/codepro/testecs/TestecsApplication.java
+++ src/main/java/com/codepro/testecs/TestecsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestecsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestecsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestecsApplication.class, args);
+    }
 
 }
--- src/test/java/com/codepro/testecs/TestecsApplicationTests.java
+++ src/test/java/com/codepro/testecs/TestecsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestecsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
